f94a327ce3a8ec4de30e3ac919cad46e292ff5ae
Jonathan <carusyte@163.com>
2024-07-07 21:20:39 +0800

    logging

--- src/softs/exp/exp_basic.py
+++ src/softs/exp/exp_basic.py
@@ -3,6 +3,7 @@ import os
 import torch
 
 from softs.models import SOFTS
+from softs.utils.tools import get_logger
 
 
 class Exp_Basic(object):
@@ -22,10 +23,10 @@ class Exp_Basic(object):
             os.environ["CUDA_VISIBLE_DEVICES"] = str(
                 self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
             device = torch.device('cuda:{}'.format(self.args.gpu))
-            print('Use GPU: cuda:{}'.format(self.args.gpu))
+            get_logger().debug("Using GPU: cuda:{}".format(self.args.gpu))
         else:
             device = torch.device('cpu')
-            print('Use CPU')
+            get_logger().debug("Using CPU")
         return device
 
     def _get_data(self):
